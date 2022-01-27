09a82de3c0f7771557a0a2a9f6ccdb09a5f8ca23
Bhartiramteke123cognitiors <52369659+Bhartiramteke123cognitiors@users.noreply.github.com>
2022-01-27 12:35:05 +0530

    Add files via upload

---
+++ face detection.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 20 15:41:37 2022
+
+@author: HP
+"""
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Read the input image
+img = cv2.imread('C:/Users/HP/Downloads/pract3.jpg')
+img
+# Convert into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray
+# Detect faces
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+faces
+#faces=face_cascade.detectMultiScale(gray_img, scaleFactor=1.05,minNeighbors=5)
+
+#faces=face_cascade.detectMultiScale(gray_img, scaleFactor=1.05,minNeighbors=5)
+# Draw rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+# Display the output
+cv2.imshow('img', img)
+cv2.waitKey()
+
+*************************
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # Display
+    cv2.imshow('img', img)
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+# Release the VideoCapture object
+cap.release()
